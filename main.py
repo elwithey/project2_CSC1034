d108f9c1cee6fc3b0b8d5457814a91681e47cdcb
elwithey <c2017382@ncl.ac.uk>
2022-10-30 15:46:32 +0000

    added to main.py so clients can be added to MOCK_DATA.csv

---
+++ main.py
@@ -0,0 +1,26 @@
+class Manage():
+
+    def __init__(self,data):
+        self.data = data
+        print()
+
+    #adds a new client to the current bank clients
+    def add(self):
+        self.data.append("")
+        print(*self.data, sep ="\n")
+
+#opens the clients' data from a csv file
+f = open("MOCK_DATA.csv","r")
+data = f.readlines()
+
+#turns data into a list
+for i in range(len(data)):
+    #takes away whitespace
+    data[i] = data[i].strip()
+
+bank = Manage(data)
+bank.add()
+
+
+
+
