fc69328548db3d1705e4140fde593bed75303b26
elwithey <c2017382@ncl.ac.uk>
2022-11-16 21:57:59 +0000

    Created testing.py and added tests

--- Testing.py
+++ Testing.py
@@ -1,12 +1,51 @@
-import testingfile as T
-#import pytest
+from main import Manage
+# import pytest
+bank = Manage()
 
 def test():
-    try:
-        T.addMoney(100,"Edward")
-    except:
-        print("Fails but not supposed to")
-    else:
-        ("YAY")
 
+    try:
+        bank.addMoney(100,"Erin")
+    except TypeError:
+        print("Fails as expected")
+    else:
+        print("Passed unexpectedly")
+
+
+def test2():
+    try:
+        bank.append("Erin","Withey","Miss","Female","8/20/2004","Student",5000,300)
+    except ValueError:
+        message = "A variable passed to the Manage class is None, please check. Aborting creation of new client."
+        print(message)
+    else:
+        print("Passed unexpectedly")
+
+
+def test3():
+    try:
+        bank.append("Erin","Withey",None,"Female","8/20/2004","Student",5000,300)
+    except ValueError:
+
+        message = "Fails as expected. A variable passed to the Manage class is None, please check. Aborting creation " \
+                  "of new client. "
+        print(message)
+    else:
+        print("yay")
+
+
+def test4():
+    try:
+        bank.append("Erin","Withey","Miss","Female","8/20/2004","Student","5000",300)
+    except TypeError:
+
+        message = "Balance is not an integer, please check. Aborting creation of new client."
+        print(message)
+    else:
+        print("yay")
+
+
+test2()
 test()
+test3()
+test4()
