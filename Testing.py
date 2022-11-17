e3f617c8039f9bce5a26fec83829db217f9f6cef
elwithey <c2017382@ncl.ac.uk>
2022-11-17 21:16:45 +0000

    Further changes made to Testing.py and main.py

--- Testing.py
+++ Testing.py
@@ -1,51 +1,105 @@
 from main import Manage
+
 # import pytest
 bank = Manage()
 
-def test():
 
+def addMoneyFail():
+    print("Runs function addMoney.Enters a non-existent user")
     try:
-        bank.addMoney(100,"Erin")
+        bank.addMoney(100, "Erin")
     except TypeError:
         print("Fails as expected")
     else:
         print("Passed unexpectedly")
 
 
-def test2():
+def addMoneyPass():
+    print("\n Runs function addMoney.Enters a user in the MOCK_DATA.csv file")
     try:
-        bank.append("Erin","Withey","Miss","Female","8/20/2004","Student",5000,300)
+        bank.addMoney(100, "Edward")
+    except TypeError:
+        print("Fails Unexpectedly. Should find client named Edward and deposit £100")
+    else:
+        print("Passed as expected")
+
+
+def addClientPass():
+    print("\nRuns function append.Enters correct information to create a new user")
+
+    try:
+        bank.append("Erin", "Withey", "Miss", "Female", "8/20/2004", "Student", 5000, 300)
     except ValueError:
         message = "A variable passed to the Manage class is None, please check. Aborting creation of new client."
         print(message)
     else:
-        print("Passed unexpectedly")
+        print("Passed as expected")
+        client = bank.searchName("Erin")
+        print(client[0],"has now been added to the list")
 
 
-def test3():
+def addClientFail1():
+    print("\nRuns function append. Enters an invalid value for title column")
+
     try:
-        bank.append("Erin","Withey",None,"Female","8/20/2004","Student",5000,300)
+        bank.append("Erin", "Withey", None, "Female", "8/20/2004", "Student", 5000, 300)
     except ValueError:
 
         message = "Fails as expected. A variable passed to the Manage class is None, please check. Aborting creation " \
                   "of new client. "
         print(message)
     else:
-        print("yay")
+        print("Passed unexpectedly")
 
 
-def test4():
+def addClientFail2():
+    print("\nRuns function addClient. Enters an a string for the balance value")
+
     try:
-        bank.append("Erin","Withey","Miss","Female","8/20/2004","Student","5000",300)
+        bank.append("Erin", "Withey", "Miss", "Female", "8/20/2004", "Student", "5000", 300)
     except TypeError:
-
-        message = "Balance is not an integer, please check. Aborting creation of new client."
+        message = "Fails as expected. Balance is not an integer, please check. Aborting creation of new client."
         print(message)
     else:
         print("yay")
 
 
-test2()
-test()
-test3()
-test4()
+def returnDOB():
+    print("\nRuns function searchDOB. Prints clients with a birthday of 10/12/2002")
+    dob = "10/12/2002"
+    test = bank.searchDOB(dob)
+    if test == "empty":
+        print("NO CLIENTS WITH THAT DATE OF BIRTH")
+    else:
+        print("Clients details with birthday", dob, "are", test)
+
+
+def deletePass():
+    print("\nRuns function delete. Deletes client with name Hayden at position 91")
+    try:
+        bank.delete("Hayden")
+        print("")
+    except ValueError:
+        message = "Unexpected fail"
+        print(message)
+    else:
+        print("Test passed as expected")
+        for i in range(0,len(bank.data)):
+            print(bank.data[i])
+
+
+def searchOverdraft():
+    print("\nRuns function searchOverdraft. Finds clients who are over their overdraft")
+    value = bank.searchOverdraft()
+    for i in range(0, len(value)):
+        print(value[i])
+
+
+addMoneyFail()
+addMoneyPass()
+addClientPass()
+addClientFail1()
+addClientFail2()
+returnDOB()
+deletePass()
+searchOverdraft()
--- main.py
+++ main.py
@@ -1,76 +1,97 @@
 import csv
 
 
-class Manage():
+class Manage:
 
     def __init__(self):
         self.data = []
+        self.read()
         self.fName = ""
 
     # retrieves a list of all data on all clients in the bank
     def retrieve(self):
-        self.read()
-        for i in range(0,len(self.data)):
+        # self.read()
+        for i in range(0, len(self.data)):
             print(self.data[i])
 
-    #appends the csv file to add a new client
+    # appends the csv file to add a new client
     def append(self, fName, sName, title, gender, DOB, occupation, balance, overdraft):
-        self.read()
-        if (self.fName == None) or (sName == None) or (title == None) or (gender == None) or (DOB == None) or (
-                occupation == None) or (balance == None) or (overdraft == None):
-            # If we find a variable which is None, raise an Exception
+        # self.read()
+        if (fName is None) or (sName is None) or (title is None) or (gender is None) or (DOB is None) or \
+                (occupation is None) or (balance is None) or (overdraft is None):
+            # If we find a variable  which is None, raise an Exception
             raise ValueError()
         if not isinstance(balance, int) or not isinstance(overdraft, int):
             raise TypeError()
 
         data_len = len(self.data)
         client_id = data_len + 1
-        self.data.append([(str(client_id)), self.fName, sName, title, gender, DOB, occupation, (str(balance)), (str(overdraft))])
-        for i in range(0, len(self.data)):
-            print(self.data[i])
-
+        self.data.append([(str(client_id)), fName, sName, title, gender, DOB, occupation, (str(balance)),
+                          (str(overdraft))])
+        # for i in range(0, len(self.data)):
+            # print(self.data[i])
 
     def read(self):
 
-        with open("MOCK_DATA.csv") as csvfile:
-            reader = csv.reader(csvfile)
+        with open("MOCK_DATA.csv", "r") as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
             for row in reader:
                 self.data.append(row)
 
-    def search(self):
-        self.read()
-            #reference
-            column = [x[1] for x in self.data]
-            if self.fname in column:
-                for x in range(0,len(self.data)):
-                    if self.fname == self.data[x][1]:
-                        return self.data[x]
+    def searchName(self, fName):
+        # self.read()
+        # reference
+        column = [x[1] for x in self.data]
+        if fName in column:
+            for x in range(0, len(self.data)):
+                if fName == self.data[x][1]:
+                    print(self.data[x])
+                    return self.data[x], x
 
+    def searchDOB(self, DOB):
+        # self.read()
+        final = []
+        # reference
+        column = [x[5] for x in self.data]
+        if DOB in column:
+            for x in range(0, len(self.data)):
+                if DOB == self.data[x][5]:
+                    final.append(self.data[x])
+        if len(final) == 0:
+            return "empty"
+        return final
 
-    def checkBalance (self):
-        balance = (self.search())
+    def searchOverdraft(self):
+        final = []
+        for x in range(0, len(self.data)):
+            overdraft = -abs(int(self.data[x][8]))
+            if float(self.data[x][7]) < overdraft:
+                final.append(self.data[x])
+        if len(final) == 0:
+            return "NO CLIENTS IN OVERDRAFT"
+        return final
+
+    def checkBalance(self, fName):
+        balance = (self.searchName(fName))
         # prints balance of client
         return balance[0][7]
 
-    def addMoney(self):
-        def addMoney(self, amount, fName):
-            information = (self.search(fName))
-            if information is None:
-                raise TypeError
-            x = int(information[0])
-            balance = int(information[7])
-            balance += amount
-            self.data[x][7] = balance
-            print(amount, "has been deposited into the account. The balance is now", balance)
+    def addMoney(self, amount, fName):
+        information = (self.searchName(fName))
+        if information is None:
+            raise TypeError
+        x = int(information[0])
+        balance = int(information[7])
+        balance += amount
+        self.data[x][7] = balance
+        print(amount, "has been deposited into the account. The balance is now", balance)
 
-
-def withdrawMoney(self):
-        information = (self.search())
+    def withdrawMoney(self, fName):
+        information = (self.searchName(fName))
         x = int(information[1])
         balance = float(information[0][7])
         overdraft = int(information[0][8])
         overdraft = -abs(overdraft)
-
 
         amount = float(input("How much would you like to withdraw from the account"))
         balance -= amount
@@ -79,27 +100,19 @@ def withdrawMoney(self):
         if balance < overdraft:
             balance -= 5
             self.data[x][7] = balance
-            print("£5 has been charged to the account for going over your overdraft limit. The balance is now ", balance)
+            print("£5 has been charged to the account for going over your overdraft limit. The balance is now ",
+                  balance)
 
-        quit = input("Would you like to perform any further actions? Y/N")
-        return quit
-
-
-    def getPassword(self):
-        return self.password
-
-    def delete(self):
-        information = (self.search())
+    def delete(self, fName):
+        information = (self.searchName(fName))
+        # if information is None:
+        # raise ValueError
         position = int(information[1])
-        print(position)
         self.data.pop(position)
-        for i in range(0, len(self.data)):
-            print(self.data[i])
-        quit = input("Would you like to perform any further actions? Y/N")
-        return quit
+        return self.data
 
-    def edit(self):
-        information = self.search()
+    def edit(self, fName):
+        information = self.searchName(fName)
         x = int(information[1])
         print("""
         WHICH DETAIL WOULD YOU LIKE TO EDIT
@@ -127,90 +140,7 @@ def withdrawMoney(self):
             self.data[x][int(choice)] = str(overdraft)
 
         print(information[0])
-        quit = input("Would you like to perform any further actions? Y/N")
-        return quit
-
-#MAIN CODE -------------------------
-if __name__ == "__main__":
-    quit = False
-    bank = Manage()
-    print("WELCOME TO THE BANK")
-    customer = input("Are you a new customer? Y/N  ")
-    if customer == "Y":
-        customer = input("Do you want to set up an account? Y/N  ")
-        if customer == "Y":
-            bank.append()
-
-    else:
-        customer = input("Are you an existing customer? Y/N  ")
-        if customer == "Y":
-            while not quit:
-                print("")
-                print("""
-                MENU
-                    1. CHECK BALANCE
-                    2. WITHDRAW MONEY
-                    3. ADD MONEY
-                Please pick 1,2,3 or 4.""")
-                choice = input("")
-                if choice == "1":
-                    bank.checkBalance()
-                    action = input("Would you like to perform any further actions? Y/N  ")
-                elif choice == "2":
-                    action = bank.withdrawMoney()
-
-                elif choice == "3":
-                    action = bank.addMoney()
-
-                if action == "N":
-                    quit = True
-        if customer == "N":
-            user = input("Are you an admin? Y?N  ")
-            password = input("Please input the admin password")
-            if user == "Y" and password == "no":
-                while not quit:
-                    print("""
-                                    MENU
-                                        1. CHECK BALANCE
-                                        2. WITHDRAW MONEY
-                                        3. ADD MONEY
-                                        4. DELETE CLIENT
-                                        5. EDIT DETAILS
-                                        6. SEE ALL CLIENTS
-                                    Please pick 1,2,3 or 4.""")
-                    choice = input("")
-                    action = ""
-                    if choice == "1":
-                        adminUser.checkBalance()
-                        action = input("Would you like to perform any further actions? Y/N  ")
-
-                    elif choice == "2":
-                        action = adminUser.withdrawMoney()
-
-                    elif choice == "3":
-                        action = adminUser.addMoney()
-                    elif choice == "4":
-                        action = adminUser.delete()
-                    elif choice == "5":
-                        action = adminUser.edit()
-                    elif choice == "6":
-                        action = adminUser.retrieve()
-                    if action == "N":
-                        quit = True
-
-            if password != adminUser.getPassword():
-                print("Incorrect Password")
-    print("Goodbye")
 
 
-
-
-
-
-
-
-
-
-
-
-
+bank = Manage()
+bank.searchOverdraft()
